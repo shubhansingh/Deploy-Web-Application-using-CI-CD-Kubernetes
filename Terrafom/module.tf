3626db07a717a81b4fccc970906e0171d0afd048
Shubhanshu Singh <35443356+toko62-shubhanshu@users.noreply.github.com>
2024-05-12 22:27:23 +0530

    Update and rename main.tf to module.tf

---
+++ Terrafom/module.tf
@@ -0,0 +1,16 @@
+# Create EKS Cluster
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-cluster"
+  cluster_version = "1.21"
+  subnets         = [aws_subnet.my_subnet.id]
+  vpc_id          = aws_vpc.my_vpc.id
+  node_groups = {
+    eks_workers = {
+      desired_capacity = 2
+      max_capacity     = 2
+      min_capacity     = 1
+      instance_type    = "t3.medium"
+    }
+  }
+}
