04fbb57452ed64f2f6ca78747aa9a239bedd730b
Shubhanshu Singh <35443356+toko62-shubhanshu@users.noreply.github.com>
2024-05-12 22:29:35 +0530

    Update resource.tf

--- Terrafom/resource.tf
+++ Terrafom/resource.tf
@@ -1,29 +1,24 @@
-# Create VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Create Subnet
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"  # Set your desired AZ
 }
 
-# Create Security Group
 resource "aws_security_group" "my_security_group" {
   vpc_id = aws_vpc.my_vpc.id
 
   // Define ingress and egress rules here
 }
 
-# Create IAM Role for Kubernetes
 resource "aws_iam_role" "eks_role" {
   name               = "eks-role"
   assume_role_policy = file("policies/eks-role-policy.json")
 }
 
-# Create IAM Role Policy Attachment
 resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
   role       = aws_iam_role.eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
